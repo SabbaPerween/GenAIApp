4b1c90ea7e733d272f292f9b63fd9348dd7abdde
SabbaPerween <144616988+SabbaPerween@users.noreply.github.com>
2025-02-06 19:55:22 +0530

    Add files via upload

---
+++ SimpleGenAi.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+##LangSmith Tracking 
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+##Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With LLAMA2")
+input_text=st.text_input("What question you have in mind?")
+
+##Call ollama llama2 model
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
